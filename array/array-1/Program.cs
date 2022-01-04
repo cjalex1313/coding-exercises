9b6deef2752d564fe0185ffe41960168b5a6235b
Alex Cioarec <cioarec.alexandru96@gmail.com>
2022-01-05 00:33:43 +0200

    Add exercise: Implement an algo that determines if a string has all unique ASCII characters

---
+++ array/array-1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Implement an algorith that determines if a string has all unique characters? Don't use anything else besides array
+// We'll be assuming ASCII characters
+
+namespace Exerciese {
+  public class Exercises {
+    public static void Main() {
+      Console.WriteLine(HasUniqueCharacters("Testt"));
+    }
+
+    public static bool HasUniqueCharacters(string sentence) {
+      bool[] arr = new bool[256];
+      if (sentence.Length > 256) {
+        return false;
+      }
+      foreach (var c in sentence)
+      {
+          int numericValue = (int)c;
+          if (arr[numericValue]) {
+            return false;
+          }
+          arr[numericValue] = true;
+      }
+      return true;
+    }
+  }
+}
