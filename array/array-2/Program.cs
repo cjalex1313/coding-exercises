64996dadde2ed6a5133013111a339709ae32012a
Alex Cioarec <cioarec.alexandru96@gmail.com>
2022-01-05 17:08:24 +0200

    Reverse a char array in C#

---
+++ array/array-2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Implement an algorithm that reverses a string
+
+namespace Array2;
+
+public class Array2 {
+  public static void Main() {
+    Console.WriteLine("Please enter your string: ");
+    char[] sentence = (Console.ReadLine() ?? "").ToCharArray();
+    Array.Reverse(sentence);
+    Console.WriteLine(sentence);
+  }
+}
