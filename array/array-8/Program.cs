d296f78b439c9764637e984cf280dd5e63cea28c
Alex Cioarec <cioarec.alexandru96@gmail.com>
2022-01-08 01:22:20 +0200

    Add solution to coding exercise 1.8

---
+++ array/array-8/Program.cs
@@ -0,0 +1,22 @@
+﻿// Given 2 strings write a method that checks if one is a rotation of the other
+
+namespace Program;
+
+public class Program {
+  public static void Main() {
+    Console.WriteLine("Input the first string");
+    string s1 = Console.ReadLine() ?? "";
+    Console.WriteLine("Input the second string");
+    string s2 = Console.ReadLine() ?? "";
+    Console.WriteLine(IsRotation(s1, s2));
+  }
+
+  private static bool IsRotation(string s1, string s2)
+  {
+    if (s1.Length == s2.Length && s1.Length > 0) {
+      string doubled = s1 + s1;
+      return doubled.Contains(s2);
+    }
+    return false;
+  }
+}
