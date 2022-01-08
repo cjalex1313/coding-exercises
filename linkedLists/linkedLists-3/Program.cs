9685e70230c129637ee65f20c1bfcaeae348225a
Alex Cioarec <cioarec.alexandru96@gmail.com>
2022-01-09 00:11:24 +0200

    Solution to exercise 1.3

---
+++ linkedLists/linkedLists-3/Program.cs
@@ -0,0 +1,40 @@
+﻿// Write a program to remove a node from the middle of a linked list only having access to that node
+
+namespace Program;
+
+public class Program{
+  public static void Main() {
+    Node head = new Node(5);
+    head.AppendToTail(1);
+    head.AppendToTail(2);
+    head.AppendToTail(3);
+    head.AppendToTail(4);
+    head.AppendToTail(5);
+    head.AppendToTail(6);
+    head.AppendToTail(7);
+    head.AppendToTail(8);
+    head.AppendToTail(9);
+    head.AppendToTail(10);
+
+    Node? toDelete = head;
+    if (toDelete.Next != null) toDelete = toDelete.Next;
+    if (toDelete.Next != null) toDelete = toDelete.Next;
+    if (toDelete.Next != null) toDelete = toDelete.Next;
+
+    RemoveNode(toDelete);
+
+    Node? current = head;
+    while(current != null) {
+      Console.WriteLine(current.Data);
+      current = current.Next;
+    }
+  }
+
+  private static void RemoveNode(Node? toDelete)
+  {
+    if (toDelete == null || toDelete.Next == null) return;
+
+    toDelete.Data = toDelete.Next.Data;
+    toDelete.Next = toDelete.Next.Next;
+  }
+}
